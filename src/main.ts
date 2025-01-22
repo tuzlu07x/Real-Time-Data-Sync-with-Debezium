54b63116270e1f260c0a79aad82ba86c86ccce5e
Fatih Tuzlu <fatihtuzlu07@gmail.com>
2025-01-22 17:41:26 +0300

    apache kafka configured to main file

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,36 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Transport } from '@nestjs/microservices';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
+  const logger = new Logger('Bootstrap');
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.connectMicroservice({
+    transport: Transport.KAFKA,
+    options: {
+      client: {
+        brokers: ['kafka:9093'],
+        clientId: 'orders-service',
+      },
+      consumer: {
+        groupId: 'orders-consumer',
+        allowAutoTopicCreation: true,
+        sessionTimeout: 45000,
+        heartbeatInterval: 15000,
+        maxBytes: 10485760,
+      },
+      subscribe: {
+        fromBeginning: true,
+      },
+    },
+  });
+
+  await app.startAllMicroservices();
+  await app.listen(3000);
+
+  logger.log('Application started');
 }
 bootstrap();
