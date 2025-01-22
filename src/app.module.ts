7451e18b2a21c566d244bc4cf40558f4851c436c
Fatih Tuzlu <fatihtuzlu07@gmail.com>
2025-01-22 17:40:48 +0300

    mongo set into app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { OrdersModule } from './orders/orders.module';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb://root:rootpassword@mongodb:27017/nest?authSource=admin',
+    ),
+    UsersModule,
+    OrdersModule,
+  ],
+
   controllers: [AppController],
   providers: [AppService],
 })
